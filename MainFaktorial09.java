2585197fa33efc2416b3bd8512d53526163ced80
unknown <bima.rafi60@sma.belajar.id>
2025-03-13 08:41:21 +0700

    1

---
+++ MainFaktorial09.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MainFaktorial09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+
+        Faktorial09 fk = new Faktorial09();
+        System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC: " + fk.faktorialDC(nilai));
+        
+    }
+    
+}
